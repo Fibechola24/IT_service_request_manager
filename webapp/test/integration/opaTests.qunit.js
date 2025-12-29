75ebd5fbf598045167375fdb3762f4eac9380ff6
Victor Fibechola Okpanachi <victor-fibechola.okpanachi@hsrw.org>
2025-12-29 20:55:23 +0000

    Initial file setup for IT manager

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["com/client/itrequestmanager/itrequestmanager/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/client/itrequestmanager/itrequestmanager/test/unit/controller/Dashboard.controller"
+], function () {
+	"use strict";
+});
